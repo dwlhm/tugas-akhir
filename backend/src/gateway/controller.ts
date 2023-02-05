64211d7ec32f8228a475279db17749ec42e14b4c
dwlhm <dwiilhamm026@gmail.com>
2023-02-05 11:50:02 +0700

    add get profil gateway

--- backend/src/gateway/controller.ts
+++ backend/src/gateway/controller.ts
@@ -1,9 +1,11 @@
 import { Gateway } from 'database/models/gateway'
+import { Gateway_Mqtt } from 'database/models/gateway-mqtt'
 import {
     Request,
     Response,
     NextFunction
 } from 'express'
+import crypto from 'node:crypto'
 
 const register = async (
     req: Request,
@@ -13,7 +15,21 @@ const register = async (
 
     try {
 
-        const gateway = await Gateway.create(req.body)
+        const client_id: string = crypto.randomBytes(4).toString('hex')
+        const client_username: string = crypto.randomBytes(4).toString('hex')
+        const client_password: string = crypto.randomBytes(8).toString('hex')
+
+        const gateway = await Gateway.create({
+            ...req.body,
+            id: client_id,
+        })
+
+        await Gateway_Mqtt.create({
+            credential: Buffer.from(`${client_username}:${client_password}`).toString('base64'),
+            topic_data: `node/${client_id}/prod/data`,
+            topic_action: `node/${client_id}/prod/action`,
+            gateway_id: client_id
+        })
 
         return res.status(201).json({
             code: 201,
@@ -40,6 +56,29 @@ const register = async (
     }
 }
 
+const profil = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    try {
+        const gateway_id: string = req.params['id']
+        const gateway = await Gateway.findByPk(gateway_id)
+
+        if (!gateway) throw new Error('404#gateway')
+
+        return res.status(200).json({
+            code: 200,
+            body: gateway
+        })
+    } catch(err) {
+        console.error('[Gateway Profil] ',err.message)
+
+        next(err)
+    }
+}
 export {
-    register
+    register,
+    profil
 }
--- backend/src/gateway/index.ts
+++ backend/src/gateway/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { Authorization } from '../user/middleware'
-import { register } from './controller'
+import { profil, register } from './controller'
 
 const router: Router = Router()
 
 router.post('/', Authorization, register)
+router.get('/:id', Authorization, profil)
 
 export default router
