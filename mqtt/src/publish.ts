61ed1706779d19300b941568c25783ead793dcfa
dwlhm <dwiilhamm026@gmail.com>
2023-02-09 13:51:14 +0700

    add publish flow

--- mqtt/src/publish.ts
+++ mqtt/src/publish.ts
@@ -1,11 +1,23 @@
 import { Client } from "aedes:client";
+import { Device_Value } from "database/models/Device_Value";
 
 const Publish_Packet = async (
     packet: any,
     client: Client
 ) => {
     try {
-        console.log('[Publish_Packet] ', packet)
+        const message = packet.payload.toString('ascii')
+        const payload = JSON.parse(message)
+
+        const device_id = payload.device.id
+
+        await Device_Value.create({
+            device_id: device_id,
+            gateway_id: client.id,
+            value: message
+        })
+
+        console.log('[Publish_Packet] ', device_id)
     } catch(err) {
         console.error('[Publish_Packet] ',err.message)
     }
