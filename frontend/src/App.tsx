9e87655a2e9c9984dbe3a3ab42edad25018695c7
dwlhm <dwiilhamm026@gmail.com>
2024-03-28 16:28:41 +0700

    remove unused dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import * as React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Plus } from "react-feather"
-import { Button, Card } from "@/components/Elements"
 import { AppProvider } from '@/providers/app'
 import { AppRouter } from './routes'
 
