c1ab180a86e6529cb38cece65c9d23cbc3cd4aef
dwlhm <dwiilhamm026@gmail.com>
2024-03-29 10:01:29 +0700

    add react-leaflet

---
+++ frontend/src/components/Slider/Slider.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type SliderProps = {
+    children: React.ReactNode
+}
+
+export const Slider = ({ children }: SliderProps ) => {
+    return(
+        <>
+            {children}
+        </>
+    )
+}
--- frontend/src/features/overview/routes/Overview.tsx
+++ frontend/src/features/overview/routes/Overview.tsx
@@ -1,7 +1,13 @@
+import { Slider } from "@/components/Slider"
 export const Overview = () => {
     return(
         <div>
             Overview
+            <Slider>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+            </Slider>
         </div>
     )
 }
